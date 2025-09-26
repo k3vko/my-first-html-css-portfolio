db269a637dcc7d93ce5ce5a3f703683c12f4e017
Kevin Tuemmers <kevintuemmers99@email.com>
2025-09-26 16:40:49 +0200

    w

---
+++ script.js
@@ -0,0 +1,8 @@
+
+import { BigBlobBg } from "BigBlobBg.module.js"
+
+let colorbg = new BigBlobBg({
+	dom: "#hero",
+	colors: ["#000000","#3F4659","#8592A7","#3d494c","#212832","#3F4659"],
+	loop: true
+})
